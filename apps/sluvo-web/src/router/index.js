5dd4d95e85897dc7d1144ed84128078acbe6f5c6
Luintal <1430689684@qq.com>
2026-04-28 23:11:24 +0800

    Add Sluvo dual-state homepage

--- apps/sluvo-web/src/router/index.js
+++ apps/sluvo-web/src/router/index.js
@@ -1,21 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import CanvasWorkspaceView from '../views/CanvasWorkspaceView.vue'
+import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/projects/:projectId/canvas',
       name: 'canvas',
       component: CanvasWorkspaceView
     },
     {
-      path: '/projects/:projectId/canvas',
-      redirect: '/'
-    },
-    {
       path: '/projects',
-      redirect: '/'
+      name: 'projects',
+      component: HomeView
     }
   ]
 })
